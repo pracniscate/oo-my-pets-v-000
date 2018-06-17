23e02511d8a03dcc767b30e9642811edda355613
Lorena <lorena.giddens@outlook.com>
2018-06-17 03:13:24 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -33,4 +33,12 @@ class Owner
     pets[:fishes] << Fish.new(name)
   end
 
+  def buy_cat(name)
+    pets[:cats] << Cat.new(name)
+  end 
+
+  def buy_dog(name)
+    pets[:dogs] << Dog.new(name)
+  end
+
 end
